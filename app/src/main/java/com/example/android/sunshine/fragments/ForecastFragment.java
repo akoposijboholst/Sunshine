e0b0a39a8c562c36642cfb91d01ffb696c2cc0ab
Eugene Boholst <justineboholst16@gmail.com>
2016-03-19 23:06:59 -0700

    Created WeatherProvider

--- app/src/main/java/com/example/android/sunshine/fragments/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/fragments/ForecastFragment.java
@@ -122,12 +122,14 @@ public class ForecastFragment extends Fragment {
         return view;
     }
 
-    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {  /* The date/time conversion code is going to be moved outside the asynctask later,
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+
+     /* The date/time conversion code is going to be moved outside the asynctask later,
          * so for convenience we're breaking it out into its own method now.
          */
 
         private String getReadableDateString(long time) {
-            // Because the API returns a unix timestamp (measured in seconds),
+             // Because the API returns a unix timestamp (measured in seconds),
             // it must be converted to milliseconds in order to be converted to valid date.
             SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
             return shortenedDateFormat.format(time);
